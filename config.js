4cbcaa50acd4eba76842212f54f0082fac907ea1
GEEKMDXINC <herukobot67@gmail.com>
2024-08-02 11:55:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/984e15c
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'Crazy Bot' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'CRAZY MD' : process.env.OWNER_NAME,
-  sessionName:  process.env.SESSION_ID === undefined ?   : process.env.SESSION_ID,
+  sessionName:  process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
   author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'CRAZY_MD' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Secktor-Md' : process.env.PACK_INFO.split(";")[1],
